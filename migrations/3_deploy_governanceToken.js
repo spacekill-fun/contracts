864fd1d0bd1531b59d4b6966bb3f11c26e16056d
Ashton Chen <cdljsj@gmail.com>
2022-08-11 21:19:25 +0800

    modify gov token script

--- migrations/3_deploy_governanceToken.js
+++ migrations/3_deploy_governanceToken.js
@@ -1,6 +1,6 @@
 const GovernanceToken = artifacts.require("GovernanceToken");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(GovernanceToken, " Space Kill King", "SKK", "1000000000000000000000000000");
+  await deployer.deploy(GovernanceToken, " Space Kill King", "SKK", "50000000000000000000000000000");
   console.log("GovernanceToken deployed: ", GovernanceToken.address);
 };
